f0d6cb04b7ae62f0f92399d4b38bc0000519ced6
Julien Budynek <julienb@gmail.com>
2021-12-23 22:55:57 +0100

    solved 23 manually

---
+++ aoc2021/d23.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+# I used https://amphipod.net/ to find a manual solution to both parts
+
+#############
+#...........#
+###D#C#D#B###
+  #C#A#A#B#
+  #########
+
+#############
+#...........#
+###D#C#D#B###
+  #D#C#B#A#
+  #D#B#A#C#
+  #C#A#A#B#
+  #########
+
+# PART 1 OK = 14415
+# PART 2 OK = 41121
