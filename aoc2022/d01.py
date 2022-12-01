06c8aa3abc9cfe89f92ae89ad88ce8d6ac147613
Julien Budynek <julienb@gmail.com>
2022-12-01 08:02:02 +0100

    2022 day 01

---
+++ aoc2022/d01.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+from boilerplate import *
+
+
+def get_cals_by_elf(input_val):
+    input_val.append("")
+
+    max_cals = []
+    cur_cal = 0
+
+    for line in input_val:
+        if line == "":
+            max_cals.append(cur_cal)
+            cur_cal = 0
+        else:
+            cur_cal += int(line)
+    s_cals = sorted(max_cals, reverse=True)
+    return s_cals
+
+
+def boom_part1(input_val, DBG=True):
+    s_cals = get_cals_by_elf(input_val)
+    return s_cals[0]
+
+
+def boom_part2(input_val, DBG=True):
+    s_cals = get_cals_by_elf(input_val)
+    return sum(s_cals[0:3])
+
+puzzle_input = read_input_file("input-d01.txt")
+
+# part 1
+
+run_func(boom_part1, puzzle_input, DBG=False)
+
+# part 2
+
+run_func(boom_part2, puzzle_input, DBG=False)
+
+# PART 1 OK = 71506
+# PART 2 OK = 209603
