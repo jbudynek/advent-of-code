1cd801008e7b4d0d97d0b59de12666e2169812d6
Julien Budynek <julienb@gmail.com>
2024-12-03 08:11:52 +0100

    feat: d01

--- aoc2024/d01.py
+++ aoc2024/d01.py
@@ -1,6 +1,3 @@
-with open("input-d01.txt", "r") as file:
-    ipt_puzzle = file.read()
-
 ipt_test = """3   4
 4   3
 2   5
@@ -8,31 +5,21 @@ ipt_test = """3   4
 3   9
 3   3"""
 
+ipt_puzzle = open("input-d01.txt").read()
+
 # ipt=ipt_test
 ipt = ipt_puzzle
 
-lines = [li for li in ipt.split("\n")]
+all_e = [int(e) for li in ipt.split("\n") for e in li.split()]
 
-l1 = []
-l2 = []
-for li in lines:
-    if li == "":
-        continue
-    [e1, e2] = li.split()
-    l1.append(int(e1))
-    l2.append(int(e2))
-
-l1 = sorted(l1)
-l2 = sorted(l2)
+l1 = sorted(all_e[::2])
+l2 = sorted(all_e[1::2])
 
 r1 = [abs(e2 - e1) for (e1, e2) in zip(l1, l2)]
 r2 = [e1 * l2.count(e1) for (e1, e2) in zip(l1, l2)]
 
-result1 = sum(r1)
-result2 = sum(r2)
-
-print(f"# Part 1 solution: {result1}")
-print(f"# Part 2 solution: {result2}")
+print(f"# Part 1 solution: {sum(r1)}")
+print(f"# Part 2 solution: {sum(r2)}")
 
 # Part 1 solution: 2375403
 # Part 2 solution: 23082277
