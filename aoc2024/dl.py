07fbc63e70b1dfb054871d8cd85c49a3903bfafc
Julien Budynek <julienb@gmail.com>
2024-12-03 08:27:25 +0100

    feat: dl argv

--- aoc2024/dl.py
+++ aoc2024/dl.py
@@ -1,4 +1,6 @@
+import datetime
 import os
+import sys
 
 import requests  # type: ignore
 from dotenv import load_dotenv
@@ -6,7 +8,14 @@ from dotenv import load_dotenv
 load_dotenv()
 session_token = os.getenv("SESSION_TOKEN")
 
-url = "https://adventofcode.com/2024/day/1/input"
+y = datetime.datetime.now().year
+d = datetime.datetime.now().day
+if len(sys.argv) > 1:
+    y = int(sys.argv[1])
+if len(sys.argv) > 2:
+    d = int(sys.argv[2])
+
+url = f"https://adventofcode.com/{y}/day/{d}/input"
 
 response = requests.get(url, cookies={"session": session_token})
 
@@ -16,6 +25,6 @@ if response.status_code == 200:
 
     with open(path_to_file, "w") as f:
         f.write(response.text)
-    print("OK")
+    print("OK", url, path_to_file)
 else:
     print("FAIL", response)
