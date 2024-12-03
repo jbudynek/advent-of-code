e4f96fd448919609b4d6233c434081a6ead9d23f
Julien Budynek <julienb@gmail.com>
2024-12-03 08:12:21 +0100

    feat: d02

--- aoc2024/d02.py
+++ aoc2024/d02.py
@@ -1,12 +1,11 @@
-with open("input.txt", "r") as file:
-    ipt_puzzle = file.read()
-
 ipt_test = """7 6 4 2 1
 1 2 7 8 9
 9 7 6 2 1
 1 3 2 4 5
 8 6 4 4 1
 1 3 6 7 9"""
+
+ipt_puzzle = open("input-d02.txt").read()
 
 # ipt=ipt_test
 ipt = ipt_puzzle
