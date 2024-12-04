3e54603937dbb295168156c6d789dc9c15834194
Julien Budynek <julienb@gmail.com>
2024-12-04 10:39:29 +0100

    feat: d04

--- aoc2024/d04.py
+++ aoc2024/d04.py
@@ -12,47 +12,33 @@ MXMXAXMASX"""
 # ipt = ipt_test.splitlines()
 ipt = open("input.txt").read().splitlines()
 
+world = {}
+for y, line in enumerate(ipt):
+    for x, c in enumerate(line):
+        world[complex(x, y)] = c
 
-def create_world_complex(lines):
-    world = {}
-    x, y = -1, -1
-    for line in lines:
-        y += 1
-        x = -1
-        for c in line:
-            x += 1
-            world[complex(x, y)] = c
-    # 8 neighbors
-    dzs = [complex(1, 1) ** i / 2 ** (i // 2) for i in range(8)]
-    return world, dzs
-
-
-world, dzs = create_world_complex(ipt)
+dzs = [complex(1, 1) ** i / 2 ** (i // 2) for i in range(8)]
 
 result1 = 0
 for z0 in world.keys():
     if world[z0] != "X":
         continue
     for dz in dzs:
-        if z0 + 3 * dz not in world:
-            continue
-        mas = world[z0 + dz] + world[z0 + 2 * dz] + world[z0 + 3 * dz]
+        mas = (
+            world.get(z0 + dz, "")
+            + world.get(z0 + 2 * dz, "")
+            + world.get(z0 + 3 * dz, "")
+        )
         if mas == "MAS":
             result1 += 1
 
 result2 = 0
-x = dzs[1::2]
+xx = dzs[1::2]
 for z0 in world.keys():
-    if (
-        world[z0] != "A"
-        or z0 + x[0] not in world
-        or z0 + x[1] not in world
-        or z0 + x[2] not in world
-        or z0 + x[3] not in world
-    ):
+    if world[z0] != "A":
         continue
-    ms1 = world[z0 + x[0]] + world[z0 + x[2]]
-    ms2 = world[z0 + x[1]] + world[z0 + x[3]]
+    ms1 = world.get(z0 + xx[0], "") + world.get(z0 + xx[2], "")
+    ms2 = world.get(z0 + xx[1], "") + world.get(z0 + xx[3], "")
     if ms1 in ("MS", "SM") and ms2 in ("MS", "SM"):
         result2 += 1
 
