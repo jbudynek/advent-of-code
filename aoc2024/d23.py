a1e7136c1e8ab6f18304f5b506d6cc29b5050f1a
Julien Budynek <julienb@gmail.com>
2024-12-23 09:11:32 +0100

    feat: d23

---
+++ aoc2024/d23.py
@@ -0,0 +1,71 @@
+import networkx as nx
+
+ipt_1_7 = """kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn""".split(
+    "\n"
+)
+
+ipt = ipt_1_7
+
+ipt = open("input-d23.txt").read().strip().split("\n")
+
+net = nx.Graph()
+for ll in ipt:
+    nodes = ll.split("-")
+    net.add_edge(nodes[0], nodes[1])
+
+
+# p1
+
+r1 = 0
+clic_clic = list(nx.enumerate_all_cliques(net))
+for clique in clic_clic:
+    if len(clique) == 3:
+        for node in clique:
+            if node[0] == "t":
+                r1 += 1
+                break
+
+# p2
+all_nodes_in_biggest_clique = set()
+biggest_clique = clic_clic[-1]
+for node in biggest_clique:
+    all_nodes_in_biggest_clique.add(node)
+
+r2 = ",".join(sorted(list(all_nodes_in_biggest_clique)))
+
+print(f"# Part 1 solution: {r1}")
+print(f"# Part 2 solution: {r2}")
+
+# Part 1 solution: 1330
+# Part 2 solution: hl,io,ku,pk,ps,qq,sh,tx,ty,wq,xi,xj,yp
